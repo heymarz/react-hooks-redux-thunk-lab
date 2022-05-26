55bc1937010be24a54345d5d73c5517f7b71d871
Marlena Tsang <Marlena345@gmail.com>
2022-05-26 10:09:24 -0400

    commit tree fdf08a3d3156dd8fbae9ecd2e0b4c2caec20a12c

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import {createStore} from "redux";
+import {Provider, applyMiddleware} from "react-redux";
+import thunkMiddleware from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools-extension"
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from "./App";
+import {catsReducer} from "./features/cats/catsSlice";
+
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunk))
+
+const store = createStore(catsReducer, composedEnhancer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById("root"));
